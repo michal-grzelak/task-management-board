88050c415f17a452f20cb53eb3b7ba2991f1d8e8
Michal Grzelak <grzelakm@outlook.com>
2021-01-27 23:12:06 +0100

    Add react-router and reorganize App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
+import { ExamplePage } from './pages/ExamplePage'
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Switch>
+                    <Route exact path="/">
+                        <ExamplePage />
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
     )
 }
 
